4694ea862f0579c698486e4416cb5d9979c5f44b
赵子杭 <16289459+zihangzhao1216@user.noreply.gitee.com>
2025-11-09 01:23:15 +0800

    The subclass Paediatrate.java has been initially completed. It follows the inheritance and encapsulation features of java, reusing the functions of the parent class while extending its attributes

--- src/Paediatrician.java
+++ src/Paediatrician.java
@@ -1,2 +1,37 @@
-public class Paediatrician {
+//File name: Paediatronom.java
+//Description: Subclass (Pediatrician), inherited from HealthProfessional
+
+public class Paediatrician extends HealthProfessional {
+    private boolean neonatologyFocus;
+    public Paediatrician() {
+        super();
+        this.neonatologyFocus = false;
+    }
+    public Paediatrician(int id, String name, int roomNumber, boolean neonatologyFocus) {
+        super(id, name, roomNumber);
+        this.neonatologyFocus = neonatologyFocus;
+    }
+    public void setNeonatologyFocus(boolean neonatologyFocus) {
+        this.neonatologyFocus = neonatologyFocus;
+    }
+    public boolean isNeonatologyFocus() {
+        return neonatologyFocus;
+    }
+    @Override
+    public void printDetails() {
+        super.printDetails();
+
+        System.out.println("Neonatology Focus: " + (neonatologyFocus ? "Yes" : "No"));
+    }
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("Paediatrician [")
+                .append("id=").append(getId())
+                .append(", name=").append(getName())
+                .append(", roomNumber=").append(getRoomNumber())
+                .append(", neonatologyFocus=").append(neonatologyFocus ? "Yes" : "No")
+                .append("]")
+                .toString();
+    }
 }
