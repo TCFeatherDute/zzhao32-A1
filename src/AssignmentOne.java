c9bfed9ea6d5df31fa6399792e1f655bc9231ad8
赵子杭 <16289459+zihangzhao1216@user.noreply.gitee.com>
2025-11-09 21:26:40 +0800

    Start transcribing AssignmentOne.java Declare a static ArrayList<Appointment>in the class to store all appointment objects. Ensure that the same reservation list is shared throughout the program.

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,2 +1,10 @@
+//File: AssignmentOne.java
+//Using classes and objects (HealthProfessional subclasses)
+//Appointment management using ArrayList and polymorphism
+//Dependencies:HealthProfessional, GeneralPractitioner, Paediatrician, Appointment
+
+import java.util.ArrayList;
+
 public class AssignmentOne {
-}
+    //Static ArrayList collection: store all reservation records globally, and use static decoration to ensure that there is a unique collection instance in the whole process
+    private static ArrayList<Appointment> appointments = new ArrayList<>();
